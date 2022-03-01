012a7b021102f9c1d7c7422bf8cb78d2a6906e0f
bares.paul@gmail.com <bares.paul@gmail.com>
2022-03-01 09:45:19 +0400

    load scenario

--- src/datastore.rs
+++ src/datastore.rs
@@ -1,11 +1,12 @@
 use crate::chunk_array::ChunkArray;
-use crate::row_mapping::{IdentityMapping, RowMapping};
-use arrow::array::{Array, ArrayRef, Float64Builder, PrimitiveArray, PrimitiveBuilder, StringArray, UInt32Builder, UInt64Builder};
+use crate::row_mapping::{IdentityMapping, IntIntMapRowMapping, RowMapping};
+use arrow::array::{Array, ArrayBuilder, ArrayRef, Float64Builder, PrimitiveArray, PrimitiveBuilder, StringArray, UInt32Array, UInt32Builder, UInt64Array, UInt64Builder};
 use arrow::datatypes::{ArrowPrimitiveType, DataType, Field, Float64Type, Schema, SchemaRef, UInt32Type, UInt64Type};
 use arrow::record_batch::RecordBatch;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::process::id;
 
 use std::sync::Arc;
 
@@ -25,6 +26,7 @@ pub struct Store {
     pub vector_by_field_by_scenario: HashMap<String, HashMap<String, ChunkArray>>,
     pub row_mapping_by_field_by_scenario: HashMap<String, HashMap<String, Box<dyn RowMapping>>>,
     pub dictionary_provider: DictionaryProvider,
+    pub primary_index: HashMap<u64, u64>, // FIXME suppose the key is a u64. Should be generic.
 }
 
 impl Store {
@@ -56,6 +58,7 @@ impl Store {
             vector_by_field_by_scenario,
             row_mapping_by_field_by_scenario,
             dictionary_provider: DictionaryProvider::new(),
+            primary_index: HashMap::new(),
         }
     }
 
@@ -87,9 +90,95 @@ impl Store {
         let dic = self.dictionary_provider.dicos
             .entry(SCENARIO_FIELD_NAME.to_string())
             .or_insert(Dictionary::new());
-        *dic.map(scenario.to_string());
+        let _ = *dic.map(scenario.to_string());
 
-        let arc = batch.schema();
+        if scenario == MAIN_SCENARIO_NAME {
+            self.load_main_scenario(scenario, batch);
+        } else {
+            for index in 0..batch.columns().len() {
+                let col = batch.column(index);
+                let schema = batch.schema();
+                let field = schema.field(index);
+
+                match field.data_type() {
+                    DataType::UInt64 => {
+                        self.build_scenario_array(col, scenario, field, UInt64Builder::new(self.array_size as usize));
+                    }
+                    DataType::UInt32 => {
+                        self.build_scenario_array(col, scenario, field, UInt32Builder::new(self.array_size as usize));
+                    }
+                    DataType::Float64 => {
+                        self.build_scenario_array(col, scenario, field, Float64Builder::new(self.array_size as usize));
+                    }
+                    DataType::Utf8 => {
+                        let arr = col.as_any().downcast_ref::<StringArray>().unwrap();
+                        let mut builder = UInt32Builder::new(arr.len());
+
+                        let fields = self.schema.fields();
+                        let key_field = fields.get(self.key_indices[0] as usize).unwrap();
+                        let key_col = self.vector_by_field_by_scenario.get(MAIN_SCENARIO_NAME).unwrap().get(key_field.name()).unwrap().array.as_ref().unwrap();
+
+                        let mut row_mapping = IntIntMapRowMapping::new();
+                        let arr = col.as_any().downcast_ref::<StringArray>().unwrap();
+                        let key_col = key_col.as_any().downcast_ref::<UInt64Array>().unwrap();
+                        let base_vector = self.vector_by_field_by_scenario.get(MAIN_SCENARIO_NAME).unwrap().get(field.name()).unwrap();
+                        let base = base_vector.array.as_ref().unwrap().as_any().downcast_ref::<UInt32Array>().unwrap();
+                        let dictionary = self.dictionary_provider.dicos
+                            .get_mut(field.name())
+                            .expect(format!("cannot find dictionary for field '{}'", field).as_str());
+                        for element in arr.iter().enumerate() {
+                            let key = key_col.value(element.0);
+                            let row = self.primary_index
+                                .get(&key)
+                                .expect(format!("Cannot find key {} in {} scenario", key, MAIN_SCENARIO_NAME).as_str());
+                            let row = *row as usize;
+                            let o = base.value(row);
+                            let current_value = arr.value(row);
+
+                            let position = dictionary
+                                .get_position(&current_value.to_string());
+                            match position {
+                                None => {
+                                    // should be mapped
+                                    let p = dictionary.map(current_value.to_string());
+                                    builder.append_value(*p).unwrap();
+                                    row_mapping.map(row as u32, element.0 as u32);
+                                }
+                                Some(p) => {
+                                    if *p != o {
+                                        // already in dictionary but current value is different
+                                        builder.append_value(*p).unwrap();
+                                        row_mapping.map(row as u32, element.0 as u32);
+                                    }
+                                }
+                            }
+                        }
+
+                        if !builder.is_empty() {
+                            self.vector_by_field_by_scenario
+                                .entry(scenario.to_string())
+                                .or_insert(HashMap::new())
+                                .entry(field.name().to_string())
+                                .or_insert_with(|| {
+                                    let mut chunk_array = Store::create_chunk_array(field.clone(), self.array_size);
+                                    chunk_array.set_array(Box::new(builder.finish()));
+                                    chunk_array
+                                });
+                            self.row_mapping_by_field_by_scenario
+                                .entry(scenario.to_string())
+                                .or_insert(HashMap::new())
+                                .entry(field.name().to_string())
+                                .or_insert_with(|| row_mapping);
+                        }
+                    }
+                    _ => { panic!("type not supported {}", field.data_type()) }
+                }
+            }
+        }
+    }
+
+    fn load_main_scenario(&mut self, scenario: &str, batch: &RecordBatch) {
+        let schema = batch.schema();
         for index in 0..batch.columns().len() {
             let col = batch.column(index);
 
@@ -98,19 +187,28 @@ impl Store {
                 *rc += col.len() as u64;
             }
 
-            let field = arc.field(index);
+            if index as u32 == self.key_indices[0] {
+                let arr = col.as_any().downcast_ref::<UInt64Array>().unwrap(); // FIXME should not be hardcoded
+                let mut r: u64 = 0;
+                for b in arr.iter() {
+                    self.primary_index.insert(b.unwrap(), r);
+                    r += 1;
+                }
+            }
+
+            let field = schema.field(index);
             match field.data_type().clone() {
                 DataType::UInt64 => {
                     let builder = UInt64Builder::new(self.array_size as usize);
-                    self.primitive::<UInt64Type>(col, scenario, field, builder);
+                    self.build_base_array::<UInt64Type>(col, scenario, field, builder);
                 }
                 DataType::UInt32 => {
                     let builder = UInt32Builder::new(self.array_size as usize);
-                    self.primitive::<UInt32Type>(col, scenario, field, builder);
+                    self.build_base_array::<UInt32Type>(col, scenario, field, builder);
                 }
                 DataType::Float64 => {
                     let builder = Float64Builder::new(self.array_size as usize);
-                    self.primitive::<Float64Type>(col, scenario, field, builder);
+                    self.build_base_array::<Float64Type>(col, scenario, field, builder);
                 }
                 DataType::Utf8 => {
                     let string_array = col.as_any().downcast_ref::<StringArray>().unwrap();
@@ -119,7 +217,7 @@ impl Store {
                         .or_insert(Dictionary::new());
                     let mut builder = UInt32Builder::new(string_array.len());
                     for element in string_array {
-                        builder.append_value(*dic.map(element.unwrap().to_string()));
+                        builder.append_value(*dic.map(element.unwrap().to_string())).unwrap();
                     }
                     self.get_chunk_array(scenario, field)
                         .set_array(Box::new(builder.finish()));
@@ -129,7 +227,7 @@ impl Store {
         }
     }
 
-    fn primitive<T: ArrowPrimitiveType>(
+    fn build_base_array<T: ArrowPrimitiveType>(
         &mut self,
         col: &ArrayRef,
         scenario: &str,
@@ -142,6 +240,52 @@ impl Store {
         let array = builder.finish();
         let chunk_array = self.get_chunk_array(scenario, field);
         chunk_array.set_array(Box::new(array));
+    }
+
+    fn build_scenario_array<T: ArrowPrimitiveType>(
+        &mut self,
+        col: &ArrayRef,
+        scenario: &str,
+        field: &Field,
+        mut builder: PrimitiveBuilder<T>) {
+        let fields = self.schema.fields();
+        let key_field = fields.get(self.key_indices[0] as usize).unwrap();
+        let key_col = self.vector_by_field_by_scenario.get(MAIN_SCENARIO_NAME).unwrap().get(key_field.name()).unwrap().array.as_ref().unwrap();
+
+        let mut row_mapping = IntIntMapRowMapping::new();
+        let arr = col.as_any().downcast_ref::<PrimitiveArray<T>>().unwrap();
+        let key_col = key_col.as_any().downcast_ref::<UInt64Array>().unwrap();
+        let base_vector = self.vector_by_field_by_scenario.get(MAIN_SCENARIO_NAME).unwrap().get(field.name()).unwrap();
+        let base = base_vector.array.as_ref().unwrap().as_any().downcast_ref::<PrimitiveArray<T>>().unwrap();
+        for element in arr.iter().enumerate() {
+            let key = key_col.value(element.0);
+            let row = self.primary_index
+                .get(&key)
+                .expect(format!("Cannot find key {} in {} scenario", key, MAIN_SCENARIO_NAME).as_str());
+            let row = *row as usize;
+            let o = base.value(row);
+            if o != arr.value(row) {
+                builder.append_value(element.1.unwrap()).unwrap();
+                row_mapping.map(row as u32, element.0 as u32)
+            }
+        }
+
+        if !builder.is_empty() {
+            self.vector_by_field_by_scenario
+                .entry(scenario.to_string())
+                .or_insert(HashMap::new())
+                .entry(field.name().to_string())
+                .or_insert_with(|| {
+                    let mut chunk_array = Store::create_chunk_array(field.clone(), self.array_size);
+                    chunk_array.set_array(Box::new(builder.finish()));
+                    chunk_array
+                });
+            self.row_mapping_by_field_by_scenario
+                .entry(scenario.to_string())
+                .or_insert(HashMap::new())
+                .entry(field.name().to_string())
+                .or_insert_with(|| row_mapping);
+        }
     }
 
     fn get_chunk_array(&mut self, scenario: &str, field: &Field) -> &mut ChunkArray {
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use arrow::datatypes::{DataType, Field, Float64Type, Schema, UInt32Type};
 use arrow::{record_batch::RecordBatch, util::pretty::print_batches};
 use crate::aggregator::{Aggregator, AggregatorAccessor, AggregatorFactory, SumFloat64Aggregator, SumUIntAggregator};
 
-use crate::datastore::{MAIN_SCENARIO_NAME, SCENARIO_FIELD_NAME, Store};
+use crate::datastore::{CHUNK_DEFAULT_SIZE, MAIN_SCENARIO_NAME, SCENARIO_FIELD_NAME, Store};
 use crate::dictionary_provider::Dictionary;
 use crate::point_dictionary::PointDictionary;
 use crate::point_list_aggregates_result::PointListAggregateResult;
@@ -29,13 +29,6 @@ mod bitmap_row_iterable_provider;
 mod row_iterable_provider;
 
 fn main() {
-    let id_array = UInt64Array::from(vec![0, 1, 2, 3, 4]);
-    let quantity_array = UInt32Array::from(vec![5, 3, 4, 1, 4]);
-    let product_array =
-        StringArray::from(vec!["syrup", "tofu", "mozzarella", "syrup", "tofu"]);
-    let category_array =
-        StringArray::from(vec!["condiment", "milk", "milk", "condiment", "milk"]);
-    let price_array = Float64Array::from(vec![2f64, 8f64, 4f64, 2f64, 8f64]);
     let schema = Schema::new(vec![
         Field::new("id", DataType::UInt64, false),
         Field::new("product", DataType::Utf8, false),
@@ -45,62 +38,16 @@ fn main() {
     ]);
 
     let schema_ref = Arc::new(schema);
-    let mut store = Store::new(schema_ref, vec![0], 4);
-    // FIXME create a utility method?
-    let batch = RecordBatch::try_new(
-        store.schema(),
-        vec![
-            Arc::new(id_array),
-            Arc::new(product_array),
-            Arc::new(category_array),
-            Arc::new(price_array),
-            Arc::new(quantity_array),
-        ],
-    )
-    .unwrap();
+    let mut store = Store::new(schema_ref, vec![0], CHUNK_DEFAULT_SIZE as u32);
 
-    store.load(MAIN_SCENARIO_NAME, &batch);
+    let main_batch = create_main_batch(&store);
+    let s1_batch = create_main_batch(&store);
 
-    // println!("Datastore: {:?}", store);
-    print_batches(&[batch]).unwrap();
+    store.load(MAIN_SCENARIO_NAME, &main_batch);
+    store.load("s1", &s1_batch);
 
-    // let mut point_dictionary: PointDictionary = PointDictionary::new(1);
-    //
-    // let factory = AggregatorFactory::new();
-    // let chunks = store.vector_by_field_by_scenario.get(MAIN_SCENARIO_NAME).unwrap();
-    // let prod = chunks.get("product").unwrap();
-    // let category = chunks.get("category").unwrap();
-    // let price = chunks.get("price").unwrap();
-    // let quantity = chunks.get("quantity").unwrap();
-    //
-    // let mut price_aggregator: Box<dyn Aggregator> = factory.create(price, "", "");
-    // let mut quantity_aggregator: Box<dyn Aggregator> = factory.create(quantity, "", "");
-    //
-    // // Try to aggregate by hand.
-    // for row in 0..*store.row_count.borrow() {
-    //     // let's aggregate by product
-    //     let value: u32 = prod.read::<UInt32Type>(row as u32);
-    //     let mut point: [u32; 1] = [0; 1];
-    //     point[0] = value;
-    //     let destination_row = point_dictionary.map(&point);
-    //     price_aggregator.aggregate(row as u32, destination_row);
-    //     quantity_aggregator.aggregate(row as u32, destination_row);
-    // }
-    // price_aggregator.finish();
-    // quantity_aggregator.finish();
-    //
-    // let point_names = vec![String::from("product")];
-    // let aggregate_names = [price, quantity].map(|a| a.field.name().clone()).to_vec();
-    // let dictionaries = point_names.iter().map(|name| store.dictionary_provider.dicos.get(name).unwrap()).collect();
-    // let result = PointListAggregateResult::new(
-    //     point_dictionary,
-    //     point_names,
-    //     dictionaries,
-    //     vec![price_aggregator.get_destination(), price_aggregator.get_destination()],
-    //     aggregate_names,
-    // );
-    //
-    // println!("{}", result);
+    println!("Datastore: {:?}", store);
+    print_batches(&[main_batch]).unwrap();
 
     let mut query = Query::new();
     let query = query
@@ -118,4 +65,46 @@ fn main() {
     // println!("{:?}", col.get_destination());
     // let col = quantity_aggregator.as_any().downcast_ref::<SumUIntAggregator>().unwrap();
     // println!("{:?}", col.get_destination());
+}
+
+fn create_main_batch(store: &Store) -> RecordBatch {
+    let id_array = UInt64Array::from(vec![0, 1, 2, 3, 4]);
+    let quantity_array = UInt32Array::from(vec![5, 3, 4, 1, 4]);
+    let product_array =
+        StringArray::from(vec!["syrup", "tofu", "mozzarella", "syrup", "tofu"]);
+    let category_array =
+        StringArray::from(vec!["condiment", "milk", "milk", "condiment", "milk"]);
+    let price_array = Float64Array::from(vec![2f64, 8f64, 4f64, 2f64, 8f64]);
+
+    RecordBatch::try_new(
+        store.schema(),
+        vec![
+            Arc::new(id_array),
+            Arc::new(product_array),
+            Arc::new(category_array),
+            Arc::new(price_array),
+            Arc::new(quantity_array),
+        ],
+    ).unwrap()
+}
+
+fn create_s1_batch(store: &Store) -> RecordBatch {
+    let id_array = UInt64Array::from(vec![0, 1, 2, 3, 4]);
+    let quantity_array = UInt32Array::from(vec![5, 4, 4, 1, 4]);
+    let product_array =
+        StringArray::from(vec!["syrup", "tofu", "mozzarella", "tofu", "tofu"]);
+    let category_array =
+        StringArray::from(vec!["condiment", "milk", "milk", "milk", "milk"]);
+    let price_array = Float64Array::from(vec![1f64, 8f64, 4f64, 2f64, 8f64]);
+
+    RecordBatch::try_new(
+        store.schema(),
+        vec![
+            Arc::new(id_array),
+            Arc::new(product_array),
+            Arc::new(category_array),
+            Arc::new(price_array),
+            Arc::new(quantity_array),
+        ],
+    ).unwrap()
 }
