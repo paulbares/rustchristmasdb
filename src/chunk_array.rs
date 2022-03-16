f160f9c6dbcd8830ef20cf4ddccb44ec97aa7554
bares.paul@gmail.com <bares.paul@gmail.com>
2022-03-16 11:53:54 +0400

    clean but 1 test is not passing

--- src/chunk_array.rs
+++ src/chunk_array.rs
@@ -20,27 +20,21 @@ pub enum ChunkArrayReader {
 impl ChunkArrayReader {
     pub fn read<T: ArrowPrimitiveType>(&self, row: u32) -> T::Native {
         match self {
-            ChunkArrayReader::BaseReader { base_array } => {
-                let array_ref = base_array.array.borrow();
-                let array = array_ref.as_ref().unwrap().as_any().downcast_ref::<PrimitiveArray<T>>().unwrap();
-                unsafe { array.value_unchecked(row as usize) }
-            }
+            ChunkArrayReader::BaseReader { base_array } => ChunkArrayReader::read_array_at_position::<T>(base_array, row),
             ChunkArrayReader::ScenarioReader { base_array, scenario_array, scenario, row_mapping } => {
                 let scenario_row = row_mapping.get(&row);
                 match scenario_row {
-                    None => {
-                        let array_ref = base_array.array.borrow();
-                        let array = array_ref.as_ref().unwrap().as_any().downcast_ref::<PrimitiveArray<T>>().unwrap();
-                        unsafe { array.value_unchecked(row as usize) }
-                    }
-                    Some(sr) => {
-                        let array_ref = scenario_array.array.borrow();
-                        let array = array_ref.as_ref().unwrap().as_any().downcast_ref::<PrimitiveArray<T>>().unwrap();
-                        unsafe { array.value_unchecked(sr as usize) }
-                    }
+                    None => ChunkArrayReader::read_array_at_position::<T>(base_array, row),
+                    Some(sr) => ChunkArrayReader::read_array_at_position::<T>(scenario_array, sr),
                 }
             }
         }
+    }
+
+    fn read_array_at_position<T: ArrowPrimitiveType>(array: &ChunkArray, row: u32) -> T::Native {
+        let array_ref = array.array.borrow();
+        let array = array_ref.as_ref().unwrap().as_any().downcast_ref::<PrimitiveArray<T>>().unwrap();
+        unsafe { array.value_unchecked(row as usize) }
     }
 
     pub fn data_type(&self) -> &DataType {
